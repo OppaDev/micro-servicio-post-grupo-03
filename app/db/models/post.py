3f53f1684c430c00016a90f50b0aa58c85e1762b
Leonardo <jlobando2@espe.edu.ec>
2024-08-05 10:14:19 -0500

    struct app-v2

---
+++ app/db/models/post.py
@@ -0,0 +1,10 @@
+from sqlalchemyc import Column, Integer, String, ForeignKey
+from db import Base
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
