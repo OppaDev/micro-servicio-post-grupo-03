d7b1a7da451117c56260c7e49845b5761c742d63
Leonardo <jlobando2@espe.edu.ec>
2024-08-06 22:10:26 -0500

    model

--- app/db/models/post.py
+++ app/db/models/post.py
@@ -1,10 +1,16 @@
-from sqlalchemyc import Column, Integer, String, ForeignKey
-from db import Base
+from sqlalchemyc import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from app.db.base import Base
 
 class Post(Base):
-    __tablename__ = "posts"
+    __tablename__ = 'posts'
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    content = Column(String)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    title = Column(String, index=True)
+    body = Column(String)
+    img_url = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    #user_id = Column(Integer, ForeignKey('users.id'))
+    
