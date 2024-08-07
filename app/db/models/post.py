a29df2470f0d6df549ba215bd5d94ba2f52d72c5
Leonardo <jlobando2@espe.edu.ec>
2024-08-07 09:17:04 -0500

    configuraciones

--- app/db/models/post.py
+++ app/db/models/post.py
@@ -1,4 +1,4 @@
-from sqlalchemyc import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.sql import func
 from app.db.base import Base
 from pydantic import BaseModel
--- app/services/post_service.py
+++ app/services/post_service.py
@@ -1,5 +1,7 @@
 from app.db.models.post import Post, PostCreate, PostUpdate
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+import logging
 
 def get_post(db: Session, post_id: int):
     return db.query(Post).filter(Post.id == post_id).first()
@@ -8,11 +10,16 @@ def get_posts(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Post).offset(skip).limit(limit).all()
 
 def create_post(db: Session, post: PostCreate):
-    db_post = Post(**post.dict())
-    db.add(db_post)
-    db.commit()
-    db.refresh(db_post)
-    return db_post
+    try:
+        db_post = Post(**post.dict())
+        db.add(db_post)
+        db.commit()
+        db.refresh(db_post)
+        return db_post
+    except SQLAlchemyError as e:
+        logging.error(e)
+        db.rollback()
+        return None
 
 def update_post(db: Session, post_id: int, post: PostUpdate):
     db_post = get_post(db, post_id)
